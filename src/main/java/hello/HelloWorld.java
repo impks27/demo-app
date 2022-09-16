1dab0607e92a757e63cc664cde13ff58e33569c7
Pradeep Kumar Singh <37983994+impks27@users.noreply.github.com>
2022-09-16 15:10:56 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -11,5 +11,29 @@ public class HelloWorld {
 
 	Greeter greeter = new Greeter();
 	System.out.println(greeter.sayHello());
+	
+	// Added for testing - Start
+	Scanner sc=new Scanner(System.in); 
+	System.out.println("Enter Username"); 
+	String user = sc.nextLine();
+
+	String user_path = ".\\Data\\"+user;
+
+	File file = new File(user_path);
+
+	try {
+	    String comm = "cmd.exe /c dir "+user_path;
+	    Process process = Runtime.getRuntime().exec(comm);
+	    BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+	    String s = null;
+	    while ((s = stdInput.readLine()) != null) {
+		System.out.println(s);
+	    }
+	}
+	catch (IOException e) {
+	    System.out.println("Error executing command");
+	}
+	// Added for testing - End
   }
 }
